9a778e104a353edae7c7724bb4f71ff87405e7ce
Hnsenchristiann <christiansensen00@gmail.com>
2021-07-23 18:20:09 +0700

    first commit

---
+++ src/apis/Comic.js
@@ -0,0 +1,15 @@
+import Api from './Api';
+
+export default {
+    auth() {
+        return Api().get("/comic/8");
+    },
+
+    comment() {
+        return Api().get("/comment");
+    },
+
+    author() {
+        return Api().get("/authors");
+    }
+};
---
+++ src/components/Author.vue
@@ -0,0 +1,38 @@
+<template>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <div class="p-1">
+        <div class="container-all">
+            <div class="all-item">
+                <div class="grid grid-cols-2">
+                    <div class="card-body">
+                        <h5 class="card-title">{{author.name}}</h5>
+                        <p class="card-text">$ {{author.description}}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios'
+    import { ref } from 'vue'
+    import Comic from "../apis/Comic";
+    export default {
+        name: 'author',
+        data() {
+            return {
+                author: []
+            };
+        },
+        mounted() {
+            Comic.author().then(response => {
+                this.author = response.data;
+            })
+        }
+    }
+</script>
+
+<style>
+
+</style>
